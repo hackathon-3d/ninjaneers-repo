6089a41dd6fac781ea0703959c41b0c1087c3877
tmoorer <tcm@Gemini2r.local>
2013-08-24 08:13:40 -0400

    current

---
+++ Spellz/Spellz/ProfileManager.m
@@ -0,0 +1,40 @@
+//
+//  ProfileManager.m
+//  Spellz
+//
+//  Created by Thomas Moorer on 8/24/13.
+//  Copyright (c) 2013 Ninjaneer. All rights reserved.
+//
+
+#import "ProfileManager.h"
+#import "AppDelegate.h"
+
+@implementation ProfileManager
+
+- (ProfileModel *)addProfile:(NSString*)name
+{
+    NSLog(@"addProfile %@", name);
+    
+    ProfileModel *newProfile = [[ProfileModel alloc] init];
+    newProfile.name = name;
+    newProfile.totalPoints = 0;
+    newProfile.wordsSpelled = [[NSMutableDictionary alloc] init];
+    
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    [delegate.defaultProfiles addObject:newProfile];
+    return newProfile;
+}
+
+- (void)removeProfile:(NSString *)name
+{
+    NSLog(@"removeProfile %@", name);
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    NSMutableArray *profiles = delegate.defaultProfiles;
+    for (int i=0; i<profiles.count; i++) {
+        if ([[profiles objectAtIndex:i] name] == name) {
+            [profiles removeObjectAtIndex:i];
+        }
+    }
+}
+
+@end
