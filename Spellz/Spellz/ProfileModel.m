c491a64650af2ab04c7373209896996b2ea40dce
Adam Mack <adammack@SparcAdmins-MacBook-Pro.local>
2013-08-23 23:32:09 -0400

    Adding Model Objects

---
+++ Spellz/Spellz/ProfileModel.m
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.m
+//  Spellz
+//
+//  Created by Adam Mack on 8/23/13.
+//  Copyright (c) 2013 Ninjaneer. All rights reserved.
+//
+
+#import "ProfileModel.h"
+
+@implementation ProfileModel
+
+@end
