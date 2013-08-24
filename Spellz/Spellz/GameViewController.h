db5dc05c2ed9dd07cf5da6f22a33863fbc73b1c3
kcamada <adammack@SparcAdmins-MacBook-Pro.local>
2013-08-24 10:28:20 -0400

    Adding game control

---
+++ Spellz/Spellz/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  Spellz
+//
+//  Created by Adam Mack on 8/24/13.
+//  Copyright (c) 2013 Ninjaneer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ Spellz/Spellz/GameViewController.m
@@ -0,0 +1,38 @@
+//
+//  GameViewController.m
+//  Spellz
+//
+//  Created by Adam Mack on 8/24/13.
+//  Copyright (c) 2013 Ninjaneer. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
